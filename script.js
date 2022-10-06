c5c4fc3452cfc1f45f3473cd32cf3ba8ee1593bc
lucasvlg <villegaslucas@gmail.com>
2022-10-06 16:57:52 -0300

    Aplicando cambios al archivo

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 //#region variables
 var num1 = 15;
 var num2 = 20;
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+var esc1 = "La suma es: ";
+var esc2 = "La resta es: ";
+var esc3 = "La multiplicación es: ";
+var esc4 = "La división es: ";
+console.log(esc1, num1 + num2);
+console.log(esc2, num1 - num2);
+console.log(esc3, num1 * num2);
+console.log(esc4, num1 / num2);
 
 //#region let
 if (true){
 let num1 = 12;
-console.log(num1);
+let esc = "El número es: "
+console.log(esc, num1);
 }
 
 //#region constantes
@@ -40,7 +45,9 @@ const mostrarnombre = function(nombres) {
 const mostraredad = function(edades) {
     return edades.edad;
 }
+const nom = "Nombres: "
+const eda = "Edades: ";
 const nombrespersonas = persona.map(mostrarnombre);
 const edadpersonas = persona.map(mostraredad);
-console.log(nombrespersonas);
-console.log(edadpersonas);
+console.log(nom, nombrespersonas);
+console.log(eda, edadpersonas);
