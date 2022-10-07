a01a64bd425b930944c51b647be2443790aaca33
lucasvlg <villegaslucas@gmail.com>
2022-10-06 22:11:22 -0300

    Ordenando el código

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ var esc1 = "La suma es: ";
 var esc2 = "La resta es: ";
 var esc3 = "La multiplicación es: ";
 var esc4 = "La división es: ";
-console.log(esc1, num1 + num2);
-console.log(esc2, num1 - num2);
-console.log(esc3, num1 * num2);
-console.log(esc4, num1 / num2);
 
 //#region let
 if (true){
-let num1 = 12;
-let esc = "El número es: "
-console.log(esc, num1);
+let num3 = 12;
+let esc = "El número es: ";
+console.log(esc, num3);
 }
 
 //#region constantes
@@ -22,7 +18,6 @@ const num4 = {
     num1: 5,
     num2: 6,
 }
-console.log(num4);
 
 //#region array
 nombre = "Lucas";
@@ -30,4 +25,11 @@ apellido = "Villegas";
 edad = "17";
 array = {nombre,apellido,edad}
 array.lenght,[0];
+
+// Ejecución
+console.log(esc1, num1 + num2);
+console.log(esc2, num1 - num2);
+console.log(esc3, num1 * num2);
+console.log(esc4, num1 / num2);
+console.log(num4);
 console.log(array);
